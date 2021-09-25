c84247f448a0e6b5bff44a0e656785332e4dedb9
ending3 <threeh1942308184@outlook.com>
2021-09-25 16:38:24 +0800

    修改了输入空白名字玩家的错误，晚上了数组的一些优化。

--- 第一阶段考核作业.py
+++ 第一阶段考核作业.py
@@ -1,4 +1,4 @@
-'''
+"""
 作业需求：
 1、生成一副扑克牌（自己设计扑克牌的结构，小王和大王可以分别用14、15表示 ）
 
@@ -18,28 +18,23 @@ result = {
     "武沛齐":9,
     "李路飞":0
 }
-'''
+"""
 
 import random
 
 # 新建一个扑克花色表
 poke_color = ["红桃", "黑桃", "方块", "梅花"]
 
-# 新建一个扑克数值列表
-num_list = []
-for i in range(1, 14):
-    num_list.append(i)
-
 # 创建一副新的扑克牌及各牌对应代表分数的列表，如[('红桃', 7 , 7),('黑桃', 12 , 0.5)...]
 total_poke_list = []
-for color in range(len(poke_color)):
+for color in poke_color:
     for num in range(1, 14):
         if num < 11:
-            data = (poke_color[color], num_list[num - 1], num_list[num - 1])
-            total_poke_list.append(data)
+            score = num
         else:
-            data = (poke_color[color], num_list[num - 1], 0.5)
-            total_poke_list.append(data)
+            score = 0.5
+        data = (color, num, score)
+        total_poke_list.append(data)
 special_poke = [('小王', 14, 0.5), ('大王', 15, 0.5)]
 total_poke_list.extend(special_poke)
 
@@ -47,14 +42,18 @@ print('游戏开始'.center(50, '-'))
 
 # 自定义录入三名玩家姓名
 user_list = []
-for i in range(3):
-    user_input = input(f"请输入玩家{i + 1}号的姓名({i + 1}/3):").strip()
-    user_list.append(user_input)
+while len(user_list) < 3:
+    user_order = len(user_list) + 1
+    user_input = input(f"请输入玩家{user_order}号的姓名({user_order}/3):").strip()
+    if user_input and user_input not in user_list:
+        user_list.append(user_input)
+    else:
+        print("输入错误，请重新输入")
 
 # 首先为三位玩家先随机各发一张牌
 poke_first = []
 poke_score = []
-for i in range(len(user_list)):
+for name in user_list:
     index = random.randint(0, len(total_poke_list) - 1)
     poke = total_poke_list.pop(index)
     poke_first.append(poke)
@@ -64,8 +63,8 @@ print("各玩家首次发牌完毕，下面是加牌环节".center(50, '-'))
 
 # 依次询问用户是否需要选择要牌，最后统计出各玩家最后得分（牌值爆了则为0分）
 result = {}
-for i in range(len(user_list)):
-    print(f"{user_list[i]}玩家，你的第一张牌是:{poke_first[i][0]}{poke_first[i][1]}")
+for index, name in enumerate(user_list):
+    print(f"{name}玩家，你的第一张牌是:{poke_first[index][0]}{poke_first[index][1]}")
     while True:
         choice = input("请选择是否需要继续要牌（y/n）：").strip()
         choice = choice.upper()
@@ -77,26 +76,26 @@ for i in range(len(user_list)):
 
         # 输入N，则选择不要牌了
         if choice == 'N':
-            result[user_list[i]] = poke_score[i]
-            print(f"{user_list[i]}玩家选择不要牌了")
+            result[user_list[index]] = poke_score[index]
+            print(f"{user_list[index]}玩家选择不要牌了")
             break
 
         # 输入Y,继续要牌，将牌得分进行累加，判断是否爆了
         if choice == 'Y':
-            index = random.randint(0, len(total_poke_list) - 1)
-            poke = total_poke_list.pop(index)
-            print(f"{user_list[i]}玩家要的牌为:{poke[0]}{poke[1]}")
-            poke_score[i] += poke[2]
+            index_random = random.randint(0, len(total_poke_list) - 1)
+            poke = total_poke_list.pop(index_random)
+            print(f"{user_list[index]}玩家要的牌为:{poke[0]}{poke[1]}")
+            poke_score[index] += poke[2]
 
             # 判断牌值得分是否爆了
-            while poke_score[i] <= 11:
-                result[user_list[i]] = poke_score[i]
+            while poke_score[index] <= 11:
+                result[user_list[index]] = poke_score[index]
                 break
             else:
-                print(f"很遗憾{user_list[i]}玩家,您手中的牌已爆！")
-                result[user_list[i]] = 0
+                print(f"很遗憾{user_list[index]}玩家,您手中的牌已爆！")
+                result[user_list[index]] = 0
                 break
 
 print("游戏结束，玩家的得分情况如下：".center(50, '-'))
-for i in result:
-    print(f"玩家{i}得分：", result[i])
+for name in result:
+    print(f"玩家{name}得分：", result[name])
